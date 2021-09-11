2b8428647fff9437560ad150996f4a45d85df2dd
Leandro Lesnik <52947547+leandrolesnik@users.noreply.github.com>
2021-09-11 15:30:01 -0300

    home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:math_expressions/math_expressions.dart';
 import 'package:flutter/material.dart';
+import '/pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:soulfenix/pages/calc_app.dart';
-import 'package:soulfenix/pages/home_page.dart';
-
 import './pages/calc_button.dart';
 
 void main() {
   runApp(App());
 }
-
 
 class App extends StatelessWidget {
   @override
@@ -18,9 +15,10 @@ class App extends StatelessWidget {
       home: HomePage(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        primaryColor: Colors.deepPurple,
-        accentColor: Colors.deepPurple[600],
+        primaryColor: Color(0xFFCC2222),
+        accentColor: Color(0xFFDDDDDD),
+        textTheme:
+            GoogleFonts.sourceSansProTextTheme(Theme.of(context).textTheme),
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,12 +10,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var cinza = Color(0xFFDDDDDD);
   var vermelho = Color(0xFFCC2222);
-  var pretro = Color(0xFF000000);
+  var preto = Color(0xFF000000);
   var branco = Color(0xFFFFFFFF);
+  var texto = TextStyle(fontWeight: FontWeight.w400);
+  var legenda = TextStyle(fontSize: 16, color: Colors.grey.shade600);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFDDDDDD),
       appBar: AppBar(
         title: Text("SoulFenix",
             style: TextStyle(
@@ -27,38 +30,46 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Container(
+            margin: EdgeInsets.only(top: 30),
             child: Text(
-              "Título",
+              "Você sabe o que \nsignifica reciclagem?",
               style: TextStyle(
                 fontSize: 32,
                 fontWeight: FontWeight.w200,
-                color: cinza,
+                color: preto,
+                wordSpacing: 5,
               ),
+              textAlign: TextAlign.center,
             ),
           ),
           Container(
             padding: EdgeInsets.all(2),
-            height: 120,
+            // height: 120,
             child: Card(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text("Imagem"),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text("Texto 1",
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text("Texto 2"),
-                          Text("Texto 3"),
-                        ],
+              child: Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                          style: texto),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Image.asset("assets/images/main_reciclagem.jpg"),
                       ),
-                    ),
-                  )
-                ],
+                      Container(
+                        width: double.maxFinite,
+                        child: Text(
+                          "legenda",
+                          style: legenda,
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
